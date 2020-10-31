542706193d99945bb08d977669936c7d14802976
Wannte <T.wannte@gmail.com>
2020-10-31 17:53:06 +0900

    feat : absent -> present, send 'spy' message
    send jpg encoded stringImg to client with 'spy' type message

--- web.py
+++ web.py
@@ -1,21 +1,40 @@
 import asyncio
 import websockets
 import facialdetector
+import numpy
 import json
 import cv2
 
 async def accept(websocket, path):
     cap, facial_detector = facialdetector.start()
     current_exp = None
+    onCamera = False
 
     while True:
         result = facialdetector.run(cap, facial_detector)
         #print(result)
         #frame = facial_detector.frame
-
         frame = facial_detector.frame
 
         if result != None:
+          
+            if not onCamera and result[0] = 'present':
+                onCamera = True
+                ret, frame = cap.read()
+
+                if ret == False:
+                    continue
+
+                encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 90]
+                _, imgencode = cv2.imencode('.jpg', frame, encode_param)
+                stringImg = numpy.array(imgencode).tobytes()
+
+                msg = {'type':'spy', 'data': {'img': stringImg}}
+                await websocket.send(json.jumps(msg))
+
+            elif result[0] = 'absent':
+                onCamera = False
+
             if current_exp != result:
                 current_exp = result
 
@@ -24,7 +43,7 @@ async def accept(websocket, path):
 
                     print(msg)
 
-                    await websocket.send(json.dumps(msg));
+                    await websocket.send(json.dumps(msg))
                     
                 except TypeError:
                     pass
@@ -35,7 +54,7 @@ async def accept(websocket, path):
 if __name__ == "__main__":
     websoc_svr = websockets.serve(accept, "localhost", 3000)
     
-    asyncio.get_event_loop().run_until_complete(websoc_svr);
-    asyncio.get_event_loop().run_forever();
+    asyncio.get_event_loop().run_until_complete(websoc_svr)
+    asyncio.get_event_loop().run_forever()
 
     print("started")
